5cf97e05e96df87ae29cc38c763eed2ae9db4d0b
sm-snfgithub <134285669+sm-snfgithub@users.noreply.github.com>
2023-05-26 18:41:47 +0530

    Update streamlt_app.py

--- streamlt_app.py
+++ streamlt_app.py
@@ -1,4 +1,9 @@
+import streamlit
 import pandas
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')amlit_app.py
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
