e11a4cd842a5cce2d145ac8852158bbc8fff1d6b
sm-snfgithub <134285669+sm-snfgithub@users.noreply.github.com>
2023-05-26 18:44:21 +0530

    Update streamlt_app.py

--- streamlt_app.py
+++ streamlt_app.py
@@ -7,3 +7,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
